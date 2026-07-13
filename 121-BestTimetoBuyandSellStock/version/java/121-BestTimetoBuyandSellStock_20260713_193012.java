a721187a70b6caae571dbc49bcd43963d7c8669f
Pravarshini <pravarshini2006@gmail.com>
2026-07-13 19:30:14 +0530

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/version/java/121-BestTimetoBuyandSellStock_20260713_193012.java
@@ -0,0 +1,23 @@
+// Last updated: 7/13/2026, 7:30:12 PM
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3
+4        int minPrice = prices[0];
+5        int maxProfit = 0;
+6
+7        for (int i = 1; i < prices.length; i++) {
+8
+9            if (prices[i] < minPrice) {
+10                minPrice = prices[i];
+11            }
+12
+13            int profit = prices[i] - minPrice;
+14
+15            if (profit > maxProfit) {
+16                maxProfit = profit;
+17            }
+18        }
+19
+20        return maxProfit;
+21    }
+22}
