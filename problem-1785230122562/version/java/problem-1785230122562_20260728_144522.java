cced51922fc78a88717de95a22a8c2e03b02aaed
Pravarshini <pravarshini2006@gmail.com>
2026-07-28 14:45:23 +0530

    Create

---
+++ problem-1785230122562/version/java/problem-1785230122562_20260728_144522.java
@@ -0,0 +1,21 @@
+// Last updated: 7/28/2026, 2:45:22 PM
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int candidate = 0;
+4        int count = 0;
+5
+6        for (int num : nums) {
+7            if (count == 0) {
+8                candidate = num;
+9            }
+10
+11            if (num == candidate) {
+12                count++;
+13            } else {
+14                count--;
+15            }
+16        }
+17
+18        return candidate;
+19    }
+20}
