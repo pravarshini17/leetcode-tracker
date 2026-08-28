107fa709e806791445fb2aab819374c9dc4e1b4e
Pravarshini <pravarshini2006@gmail.com>
2026-08-28 11:36:14 +0530

    Create 147-InsertionSortList

---
+++ 147-InsertionSortList/version/java/147-InsertionSortList_20260828_113612.java
@@ -0,0 +1,15 @@
+// Last updated: 8/28/2026, 11:36:12 AM
+1class Solution {
+2    public ListNode middleNode(ListNode head) {
+3
+4        ListNode slow = head;
+5        ListNode fast = head;
+6
+7        while (fast != null && fast.next != null) {
+8            slow = slow.next;
+9            fast = fast.next.next;
+10        }
+11
+12        return slow;
+13    }
+14}
