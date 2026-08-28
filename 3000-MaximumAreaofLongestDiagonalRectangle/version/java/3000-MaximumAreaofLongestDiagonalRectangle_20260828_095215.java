7282ef236d2e4110a5e649231abf5b9dd17988c5
Pravarshini <pravarshini2006@gmail.com>
2026-08-28 09:52:16 +0530

    Create 3000-MaximumAreaofLongestDiagonalRectangle

---
+++ 3000-MaximumAreaofLongestDiagonalRectangle/version/java/3000-MaximumAreaofLongestDiagonalRectangle_20260828_095215.java
@@ -0,0 +1,29 @@
+// Last updated: 8/28/2026, 9:52:15 AM
+1class Solution {
+2    public int areaOfMaxDiagonal(int[][] dimensions) {
+3
+4        int maxDiagonal = 0;
+5        int maxArea = 0;
+6
+7        for (int[] rectangle : dimensions) {
+8
+9            int length = rectangle[0];
+10            int width = rectangle[1];
+11
+12            int diagonal = length * length + width * width;
+13            int area = length * width;
+14
+15            if (diagonal > maxDiagonal) {
+16                maxDiagonal = diagonal;
+17                maxArea = area;
+18            }
+19
+20     
+21            else if (diagonal == maxDiagonal) {
+22                maxArea = Math.max(maxArea, area);
+23            }
+24        }
+25
+26        return maxArea;
+27    }
+28}
